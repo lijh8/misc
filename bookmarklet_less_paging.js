a6b2ceeb5f1f94438131eb8b145d14a9c5e986e7
lijh8 <9011764+lijh8@users.noreply.github.com>
2026-02-14 05:24:42 +0800

    Update bookmarklet_less_paging.js

--- bookmarklet_less_paging.js
+++ bookmarklet_less_paging.js
@@ -1,15 +1,18 @@
 javascript: (function () {
-    console.log('Bookmarklet: less paging with OFFSET: ' + OFFSET);
-    const OFFSET = 150;
+    console.log('adjust offset for less paging');
+    const OFFSET = 250;
     const MIN_SCROLL = 80;
     function h(e) {
         if (e.key === 'PageDown' || e.key === 'PageUp') {
             e.preventDefault();
             let a = window.innerHeight - OFFSET;
             a = a < MIN_SCROLL ? MIN_SCROLL : a;
-            window.scrollBy({ top: e.key === 'PageDown' ? a : -a, behavior: 'smooth' })
+            window.scrollBy({ top: e.key === 'PageDown' ? a : -a, behavior: 'smooth' });
         }
-    } if (window.__pageScrollHandler) document.removeEventListener('keydown', window.__pageScrollHandler);
+    }
+    if (window.__pageScrollHandler) {
+        document.removeEventListener('keydown', window.__pageScrollHandler);
+    }
     window.__pageScrollHandler = h;
     document.addEventListener('keydown', h);
 })();
