fc1c606b56205b43bbef7cd0a37edd4c2e2345af
lijh8 <9011764+lijh8@users.noreply.github.com>
2026-02-20 21:17:09 +0800

    Add files via upload

--- bookmarklet_toggle_theme.js
+++ bookmarklet_toggle_theme.js
@@ -1,12 +1,27 @@
 javascript: (function () {
     const id = 'bookmarklet-toggle-theme';
     let e = document.getElementById(id);
-    if (e && e.tagName === 'STYLE') {
+    if (e) {
         e.remove();
     } else {
         let s = document.createElement('style');
         s.id = id;
-        s.textContent = 'html{filter:invert(1) hue-rotate(180deg) saturate(0.5) brightness(0.9)!important;}img,video,canvas,iframe{filter:invert(1) hue-rotate(180deg) saturate(0.5) brightness(0.9)!important;}';
+        s.textContent = `
+            :root {
+                --bg-color: #212529;
+                --fg-color: #f8f9fa;
+            }
+            html {
+                filter: invert(1) grayscale(1) !important;
+                background: var(--bg-color) !important;
+            }
+            body {
+                background: var(--fg-color) !important;
+            }
+            img, video, iframe, [style*="background-image"] {
+                filter: invert(1) grayscale(1) !important;
+            }
+        `;
         document.head.appendChild(s);
     }
 })();
