3fb0ea20744b3c489bf454ed5a62112e9a209b8a
lijh8 <9011764+lijh8@users.noreply.github.com>
2026-02-21 17:01:07 +0800

    Add files via upload

--- bookmarklet_toggle_theme.js
+++ bookmarklet_toggle_theme.js
@@ -6,11 +6,8 @@ javascript: (function () {
     } else {
         let s = document.createElement('style');
         s.id = id;
-        s.textContent = 'html, iframe {'
-            + '    filter: invert(1) grayscale(1) !important;'
+        s.textContent = 'html {'
             + '    background-color: white !important;'
-            + '}'
-            + '[style*="background-image"], img, svg, video {'
             + '    filter: invert(1) grayscale(1) !important;'
             + '}';
         document.head.appendChild(s);
